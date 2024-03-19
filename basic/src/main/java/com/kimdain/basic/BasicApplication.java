00860274b3ffb050688e98ffa643add2f2a1d165
Kim_dain <ekdls15937@naver.com>
2024-03-19 16:50:36 +0900

    2024-03-19 16:50

---
+++ basic/src/main/java/com/kimdain/basic/BasicApplication.java
@@ -0,0 +1,13 @@
+package com.kimdain.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication   //  <-- basic package 확인
+public class BasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicApplication.class, args);
+	}
+
+}
