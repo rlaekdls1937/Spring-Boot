474102f7021221f7c1e7ece2007f1251f8087117
Kim_dain <ekdls15937@naver.com>
2024-03-20 17:14:26 +0900

    2024.03.20 17:14

--- basic/src/main/java/com/kimdain/basic/controller/BasicController.java
+++ basic/src/main/java/com/kimdain/basic/controller/BasicController.java
@@ -18,8 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 // @RestController = @Controller + @ResponseBody
 @RestController
 // @RequestMapping():HTTP 요청에 클래스와 메서드를 매핑하기 위한 어노테이션
-// @RequestMapping(value="/main",method={RequestMethod.GET})
 // HTTP GET localhost:4000/main/**
+// @RequestMapping(value="/main",method={RequestMethod.GET})
+
 @RequestMapping("/main")
 public class BasicController {
 
