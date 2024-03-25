432dc820acf634b48854e5c66cf2cd2ce2fb5c57
Kim_dain <ekdls15937@naver.com>
2024-03-25 17:48:45 +0900

    2024.03.25 17:48

---
+++ basic/src/main/java/com/kimdain/basic/service/implement/BasicServiceImplement.java
@@ -0,0 +1,29 @@
+package com.kimdain.basic.service.implement;
+
+import org.springframework.stereotype.Service;
+
+import com.kimdain.basic.service.BasicService;
+// Service 레이어:
+// - 실제 비즈니스 로직 (연산)을 실행하는 영역
+// - 트랜잭션 처리나 유효셩 검사(직전 비즈니스 로직 결과에 대한)를 수행하기도 함 
+// - Controller로 부터 받은 요청 데이터에 대해서 필요에 따라 Repository등을 거쳐 연산을 진행하고 Controller에 응답에 대한 데이터를 반환
+
+// @Service : 해당 클래스를 Spring Bean으로 등록하는 어노테이션, 역할은 @Component와 동일
+// @Component : 해당 클래스를 Spring Bean으로 등록하는 어노테이션
+// Spring Bean : 제어의 역전을 통해서 의존성 주입시 해당 클래스의 인스턴스를 Spring Framework가 제어하는 요소 
+@Service
+public class BasicServiceImplement implements BasicService{
+
+    @Override
+    public String getHello() {
+        return "Hello SpringBoot!!";
+        
+    }
+
+    @Override
+    public String getApple() {
+        
+            return "Get Mapping으로 만든 메서드";
+    }
+        
+}
