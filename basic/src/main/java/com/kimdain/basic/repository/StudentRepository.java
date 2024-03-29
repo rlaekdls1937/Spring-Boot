b1f4b09e85b6d94c74259796db69715f4f10a498
Kim_dain <ekdls15937@naver.com>
2024-03-29 16:58:47 +0900

    2024-03-29

--- basic/src/main/java/com/kimdain/basic/repository/StudentRepository.java
+++ basic/src/main/java/com/kimdain/basic/repository/StudentRepository.java
@@ -1,11 +1,12 @@
 package com.kimdain.basic.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.kimdain.basic.entity.StudentEntity;
 import java.util.List;
-
 
 // Repository 레이어 :
 // - 데이터베이스와 관련된 작업들을 처리하는 영역
@@ -49,4 +50,58 @@ extends JpaRepository<StudentEntity,Integer> {
 
     // address가 '서울특별시' 이면서 graduation이 true인 레코드가 존재하는가?
     boolean existsByAddressAndGraduation(String address, Boolean graduation);
+
+    // @Query :
+    // - 쿼리 메서드 생성 방식만으로는 실제 SQL 작성하는 데 한계있음
+    // - 쿼리 메서드는 복잡한 쿼리, 조인, 서브쿼리, 그룹화를 사용할 수 없음
+    // - 직접 SQL문으로 쿼리를 생성하도록 하는 어노테이션
+
+    // 예)
+    // SELETE * FROM student
+    // WHERE student_number = 5
+    // AND age > 20; 
+
+    // JPQL (Java Persistence Query Language) :
+    // - 표준 SQL과 매우 흡사하지만 Entity명과 Entity속성으로 쿼리를 작성하는 방법
+    @Query(value=
+        "SELECT s FROM student s WHERE s.studentNumber = ?1 AND s.age > ?2",
+        nativeQuery=false
+    )
+    List<StudentEntity> getStudent2(Integer studentNumber, Integer age);
+
+    // Native SQL :
+    // - 현재 사용하고 있는 RDBMS의 SQL 문법을 그대로 따르는 방식 
+    @Query(value=
+        "SELECT "+
+        "student_number AS studentNumber, " + 
+        "name, " +
+        "age, " + 
+        "address, " + 
+        "graduation " +
+        "FROM student " +
+        "WHERE student_number = ?1 " +
+        "AND age > ?2 ",
+        nativeQuery=true
+    )
+    List<StudentEntity> getStudent(Integer studentNumber, Integer age);
+
+    @Query(value=
+            "SELECT "+
+            "student_number AS studentNumber, " + 
+            "name, " +
+            "age, " + 
+            "address, " + 
+            "graduation " +
+            "FROM student " +
+            "WHERE student_number = :student_number " +
+            "AND age > :age ",
+            nativeQuery=true
+        )
+        List<StudentEntity> getStudent3(
+            @Param("student_number") Integer studentNumber,
+            @Param("age") Integer age);
+
+
 }
+
+
