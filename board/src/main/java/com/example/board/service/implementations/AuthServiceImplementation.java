12b02ad96054bc2498145dd809a47a0241dfb578
Kim_dain <ekdls15937@naver.com>
2024-04-03 17:49:18 +0900

    2024.04.03 17:50

--- board/src/main/java/com/example/board/service/implementations/AuthServiceImplementation.java
+++ board/src/main/java/com/example/board/service/implementations/AuthServiceImplementation.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.board.dto.request.auth.SignUpRequestDto;
 import com.example.board.dto.response.ResponseDto;
+import com.example.board.entity.UserEntity;
 import com.example.board.service.AuthService;
 
 @Service
@@ -12,14 +13,42 @@ public class AuthServiceImplementation implements AuthService{
 
     @Override
     public ResponseEntity<ResponseDto> signUp(SignUpRequestDto dto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'signUp'");
+    try {
+        // 1. 입력받은 이메일이 테이블에 존재하는지 조회 
+        // email -> (true, false)
+
+        // 1-1. 1에 대한 조회결과가 true 중복된 이메일 응답 처리
+
+        // 2. 입력받은 닉네임이 유저 테이블에 존재하는지 조회 
+        // nickname -> (true, false)
+
+        // 2-1. 2에 대한 조회결과가 true 중복된 닉네임 응답 처리
+
+        // 3. 입력받은 휴대전화번호이 유저 테이블에 존재하는지 조회 
+        // telNumber -> (true, false)
+
+        // 3-1. 3에 대한 조회결과가 true 중복된 휴대전화번호 응답 처리
+
+        // 4. 유저 레코드 삽입 
+        // 4-1. 유저 엔터티의 인스턴스 생성
+        // (dto) ->  uesrEntity 인스턴스
+
+        // 4-2. 유저 엔터티 인스턴스 저장
+
+        // 5. 성공 응답 처리
+
+
+    } catch (Exception exception) {
+        // 데이터베이스 작업중에 발생하는 예외 데이터베이스 에러 처리
+        
+    }
+        
+        
     }
 
     @Override
     public ResponseEntity<String> response(ResponseDto dto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'response'");
+        
     }
     
 }
